4aa8049f20540d6aa30f38e9960ab2673cc95831
nnatanata <shofirnata26@gmail.com>
2025-04-29 00:02:39 +0700

    Update Student.java

--- Pertemuan6 Polimorfisme & Binding/Analisis Code Topik Person & Student/Student.java
+++ Pertemuan6 Polimorfisme & Binding/Analisis Code Topik Person & Student/Student.java
@@ -1,3 +1,10 @@
+/*
+    Nama File   : Student.java
+    Deskripsi   : Kelas turunan dari Person yang merepresentasikan seorang mahasiswa dengan perilaku tidur yang berbeda.
+    Pembuat     : Code dari modul
+    Tanggal     : 22 April 2025
+*/
+
 public class Student extends Person
 {
     public Student(String name) {
